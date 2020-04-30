a4592d635f5dacbaa77dcdf57998a8a73009cd97
Marcos Echagüe <marcos.echague@gmail.com>
2020-04-30 19:23:23 -0400

    Agrega servidor de registro y descubrimiento Eureka

---
+++ eureka-server/src/test/java/io/github/jokoframework/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jokoframework.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
