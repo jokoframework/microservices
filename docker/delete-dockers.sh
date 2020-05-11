e9c6b8f7e8ae6120edcac51b44ee351d203103f1
mfare <mfare@sodep.com.py>
2020-05-11 10:46:20 -0400

    Asigna nombre fijo a cada docker

--- docker/delete-dockers.sh
+++ docker/delete-dockers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker ps -a
 
-docker rm -f docker_eureka-server_1 docker_config-server_1 docker_microservice2_1 docker_microservice1_1 docker_cloud-gateway_1
+docker rm -f eureka-server  config-server  microservice2  cloud-gateway  microservice1
 docker rmi joko/cloud-gateway:latest joko/microservice1:latest joko/config-server:latest joko/eureka-server:latest
 
 docker ps -a
